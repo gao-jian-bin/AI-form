b58965fff6a26d06ae59211ddfc93ca131942416
gao-jian-bin <1820474883@qq.com>
2026-09-03 17:07:09 +0800

    feat: show progress during route navigation

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
   <a class="skip-link" href="#main-content">跳到主要内容</a>
+  <NuxtLoadingIndicator
+    :height="3"
+    :throttle="0"
+    color="var(--tertiary)"
+    error-color="var(--danger)"
+  />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
