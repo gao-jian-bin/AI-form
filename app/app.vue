e67726c0c27234bf0e97844e9c537d11c32d0981
gao-jian-bin <1820474883@qq.com>
2026-09-17 17:41:37 +0800

    fix: show route progress only for slow navigation

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
   <a class="skip-link" href="#main-content">跳到主要内容</a>
   <NuxtLoadingIndicator
     :height="3"
-    :throttle="80"
+    :throttle="300"
     :hide-delay="0"
     color="var(--tertiary)"
     error-color="var(--danger)"
